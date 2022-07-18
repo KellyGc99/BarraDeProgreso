35adb615b9fd26be27a6bea6cdb4e03bf28da2c7
KellyGc99 <gutierrezkelly88@gmail.com>
2022-07-18 07:03:50 -0500

    barra de progreso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Pasos } from "./components/Progresive";
+import "./components/Progress.css";
 
-function App() {
+export function App() {
+  const [pasos] = useState(7);
+  const [activo, setActivo] = useState(0);
+  const [avance, setAvance] = useState(0);
+  useEffect(() => {
+    setAvance((100 / (pasos - 1)) * activo);
+  }, [pasos, activo]);
+  const arr = [];
+  console.log(activo);
+  for (let i = 0; i < pasos; i++) {
+    arr.push(
+      <Pasos className={i <= activo ? "Circulos active" : "Circulos"} key={i}>
+        {i + 1}
+      </Pasos>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="Content">
+          <div className="Progressbar">
+            <div className="Progress" style={{ width: avance + "%" }}></div>
+            {arr}
+          </div>
+          <div className="Button">
+            <button
+              className="Prev_Btn"
+              disabled={activo > 0 ? false : true}
+              onClick={() => {
+                activo <= 0 ? setActivo(0) : setActivo(activo - 1);
+              }}
+            >
+              Anterior
+            </button>
+            <button
+              className="Next_Btn"
+              disabled={activo >= pasos - 1 ? true : false}
+              onClick={() => {
+                activo >= pasos ? setActivo(pasos) : setActivo(activo + 1);
+              }}
+            >
+              Siguiente
+            </button>
+          </div>
+        </div>
+    </>
   );
 }
 
-export default App;
+// import { Progress } from "antd";
+// import "antd/dist/antd.min.css";
+// //import { red, green } from "@ant-design/colors";
+
+// export function App() {
+//   const [pasoActual, actualizarPasoActual] = useState(1);
+//   return (
+//     <>
+//       <h1>Hello , si sirvo al menos para esto :c</h1>
+//       <p>Seleccione los pasos: {pasoActual}</p>
+//       <div className="container">
+//         <Progress percent={100} steps={7} />
+//         {/* <br />
+//         <Progress percent={30} steps={5} />
+//         <br />
+//         <Progress percent={100} steps={5} size="small" strokeColor={green[6]} />
+//         <br />
+//         <Progress
+//           percent={60}
+//           steps={5}
+//           strokeColor={[green[6], green[6], red[5]]}
+//         /> */}
+//       </div>
+//     </>
+//   );
+// }
---
+++ src/components/Progresive.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Pasos({className,children}){
+    return (
+        <div className={className}>{children}</div> 
+    )
+}
